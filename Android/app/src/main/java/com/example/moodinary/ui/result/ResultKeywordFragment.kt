bf1274e1810baf134dae2c857eaf7a33f911f867
leastzero <csong7177@naver.com>
2025-03-10 23:16:03 +0900

    [feat] Keyword UI 구현, Emotion Btn Drawable 수정

---
+++ Android/app/src/main/java/com/example/moodinary/ui/result/ResultKeywordFragment.kt
@@ -0,0 +1,7 @@
+package com.example.moodinary.ui.result
+
+import androidx.fragment.app.Fragment
+import com.example.moodinary.R
+
+class ResultKeywordFragment : Fragment(R.layout.fragment_result_keyword) {
+}
